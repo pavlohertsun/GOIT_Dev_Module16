61b9357e4c496cb494e442fbd221b581b0ada36f
pavlo_hertsun <pavlo.hertsun.pz.2021@lpnu.ua>
2024-05-13 02:30:22 +0300

    Finished

--- src/main/java/com/example/goit_dev_module16/repositories/NoteRepository.java
+++ src/main/java/com/example/goit_dev_module16/repositories/NoteRepository.java
@@ -1,2 +1,10 @@
-package com.example.goit_dev_module16.repositories;public class NoteRepository {
+package com.example.goit_dev_module16.repositories;
+
+import com.example.goit_dev_module16.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
 }
